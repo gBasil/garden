4e326c6ff55380add3f87affa2642ce805c639e4
Basil <basil@gbasil.dev>
2022-04-11 20:14:45 -0600

    ➕ Added react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { CssBaseline, GeistProvider } from '@geist-ui/core';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return (
@@ -11,9 +14,14 @@ const App = ({ Component, pageProps }: AppProps) => {
 					'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🪴</text></svg>'
 				)}`}
 			></link>
+			
 			<GeistProvider>
 				<CssBaseline />
-				<Component {...pageProps} />
+				<QueryClientProvider client={queryClient}>
+					<Hydrate state={pageProps.dehydratedState}>
+						<Component {...pageProps} />
+					</Hydrate>
+				</QueryClientProvider>
 			</GeistProvider>
 		</>
 	);
